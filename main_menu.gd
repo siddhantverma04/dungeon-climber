c1a525fa5093c312eeececa27f0a5b30ac2a4f2d
siddhantverma04 <siddhantverma04@gmail.com>
2026-08-10 14:43:01 -0700

    Finished main menu
    Added an animation for the game to start and tweaked text.

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,11 @@
 extends Node
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +14,11 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	animation_player.play("new_animation")
+	timer.start()
+
+
+
+
+func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
