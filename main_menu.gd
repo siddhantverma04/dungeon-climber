079f51b8e6e7c0f04cf0380cfe2b2fd67526c4cc
siddhantverma04 <siddhantverma04@gmail.com>
2026-08-11 17:21:58 -0700

    Main menu animation

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,7 @@
 extends Node
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var timer: Timer = $Timer
+@onready var control_next_button: Button = $"Control next button"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,10 +16,20 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	animation_player.play("new_animation")
+	
+
+	
+
+func _on_control_next_button_pressed() -> void:
+	animation_player.play("how to play")
+
+
+func _on_how_to_play_next_pressed() -> void:
+	animation_player.play("start")
 	timer.start()
-
-
-
+	
+	
+	
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
